ed5c863eec1baa84acf0cac9b1a726fac013aef0
meurtphy <erwanmpn5@gmail.com>
2024-10-21 17:25:01 +0000

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote using the puts function
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+for(int x = 'A'; x <= 'Z'; x++
+x = tolower(x)
+putchar(x)
+}
+
+return 0;
+
+}
