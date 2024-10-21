85229a48bc498a3a56d2dfa034dd1d1298e1c9f2
meurtphy <erwanmpn5@gmail.com>
2024-10-21 09:53:04 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-/** 
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+/**
+ * main - Entry point of the program
+ *
+ * description: Prints a quote using the puts function
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }
