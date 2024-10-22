662c1524ecfea5d2e4565d3b3e32d169a103e2a5
meurtphy <erwanmpn5@gmail.com>
2024-10-22 09:56:25 +0000

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -13,11 +13,14 @@ int main(void)
 {
 int x;
 
-for (x = 0 ; x <= 15 ; x++)
+for (x = 0 ; x <= 9 ; x++)
 {
 putchar(x + '0');
 }
-
+for (x = 'a' ; x <= 'f' ; x++)
+{
+putchar(x);
+}
 putchar('\n');
 
 return (0);
