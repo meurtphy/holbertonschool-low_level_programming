b272e9a89f92ae3a9b26ff394a4c3c82646748b8
meurtphy <erwanmpn5@gmail.com>
2024-10-22 08:14:13 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints the alphabet in uppercase
+ *
+ * Description: This program prints the alphabet in uppercase,
+ * followed by a new line. Only putchar can be used.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+
+for (x = 0 ; x <= 10 ; c++)
+{
+putchar(0);
+}
+
+putchar('\n');
+
+return (0);
+}
