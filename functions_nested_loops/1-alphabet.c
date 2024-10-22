5fd698936ac73e363a1a1f645574e763ce009850
meurtphy <erwanmpn5@gmail.com>
2024-10-22 13:41:36 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
- */
+* main -  prints _putchar, followed by a new line
+*
+*Description: Prints a quote using the puts function
+* Return: Always 0 (Success)
+*/
 void print_alphabet(void)
 {
 char i;
