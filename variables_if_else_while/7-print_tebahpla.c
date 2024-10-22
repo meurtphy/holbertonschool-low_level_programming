053fbfe6a38c17ca68e8324393b5b7c1d168987e
meurtphy <erwanmpn5@gmail.com>
2024-10-22 09:29:35 +0000

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints the alphabet in uppercase
+ *
+ * Description: This program prints the alphabet in uppercase,
+ * followed by a new line. Only putchar can be used.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'z' ; c <= 'a' ; c++)
+{
+putchar(c);
+}
+
+putchar('\n');
+
+return (0);
+}
