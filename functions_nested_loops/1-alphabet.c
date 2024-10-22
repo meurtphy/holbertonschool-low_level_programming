9c2204dd550ca21cb0f8d3f747804074f347b4a6
meurtphy <erwanmpn5@gmail.com>
2024-10-22 13:02:16 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+for (int i = 0; i < 26; i++)
+{
+putchar(alphabet[i];
+}
+
+putchar('\n');
+
+return (0);
+}
