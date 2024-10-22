c0bd6bc8c31ae32b876c97b8f6de75ff9debf5c7
meurtphy <erwanmpn5@gmail.com>
2024-10-22 10:32:41 +0000

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints the alphabet in uppercase
+ *
+ * Description: This program prints the alphabet in uppercase,
+ * followed by a new line. Only putchar can be used.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+
+for (num = 0 ; num <= 9 ; num++)
+{
+putchar(num + '0');
+if (num != 9)
+{
+putchar(',')
+putchar(' ')
+}
+}
+Putchar('\n');
+
+return (0);
+}
