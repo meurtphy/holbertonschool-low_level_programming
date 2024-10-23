cb0d8383fa070e0d5d1f8b0ae8b8beeaa27f6c40
meurtphy <erwanmpn5@gmail.com>
2024-10-23 11:46:42 +0000

    main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,7 +5,7 @@ void print_alphabet_x10(void);
 void jack_bauer(void);
 void times_table(void);
 void print_to_98(int n);
-int (char c);
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
