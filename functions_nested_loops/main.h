0009280d2b53af7745a482dd6715b7471ca55deb
meurtphy <erwanmpn5@gmail.com>
2024-10-23 11:28:07 +0000

    10-add.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,10 +3,12 @@
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void jack_bauer(void);
+void times_table(void);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+int add(int, int);
 #endif
