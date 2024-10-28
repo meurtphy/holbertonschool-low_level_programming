3b94918c4476dbdf53c15d5c733e92fad90de06f
meurtphy <erwanmpn5@gmail.com>
2024-10-28 08:55:39 +0000

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
-int _isupper(int c);
+/**
+* _isupper - Verifie si un caractere est en majuscule
+* @c: Le caractere a verifer
+*
+*return 1 si c est en majuscule sinon, 0 sinon
+*/
+int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
+{
 return (1);
+}
 else
+{
 return (0);
+}
 }
 int main(void)
 {
 char c;
 
-c= 'A';
+c = 'A';
 printf("%c: %d\n", c, _isupper(c));
 c = 'a';
 printf("%c: %d\n", c, _isupper(c));
