4b58a8f80ebde12e9167f9d0cf4934be754d7eda
meurtphy <erwanmpn5@gmail.com>
2024-10-28 08:29:27 +0000

    more_functions_nested_loops

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main void
+int _isdigit(int c);
+{
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+return (0);
+else
+return (1);
+}
