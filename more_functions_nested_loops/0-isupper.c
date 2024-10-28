4ab3fbe31b91721fae5bf37fbf442d9e345f6260
meurtphy <erwanmpn5@gmail.com>
2024-10-28 08:40:40 +0000

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
-int main void
-int _isdigit(int c);
+int _isupper(int c);
 {
 if (c >= 'A' && c <= 'Z')
 return (1);
 else
 return (0);
+}
+int main(void)
+{
+char c;
+
+c= 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+
+return (0);
+
 }
