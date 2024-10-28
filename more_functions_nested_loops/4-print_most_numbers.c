ca122d04f4d1530dab550b3f4f7af9f558122112
meurtphy <erwanmpn5@gmail.com>
2024-10-28 13:54:42 +0000

    5-more_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_number - Prints the number from 0 to 9, followed by a new line
+ * print_most_number - Prints the number from 0 to 9, followed by a new line
  *
  * excluding 2 and 4
  */
--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,10 +4,15 @@
 */
 void print_numbers(void)
 {
-int i;
-for (i = '0'; i <= '14'; i++)
+int i, j;
+for (i = 0; i < 10; i++)
 {
-_putchar(i);
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+_putchar('1');
+putchar((j % 10) + '0');
 }
-_putchar('\n');
+_putchar ('\n');
+}
 }
