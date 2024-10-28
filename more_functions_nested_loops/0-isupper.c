2619a95609039be1bd2477dce3822986537b88df
meurtphy <erwanmpn5@gmail.com>
2024-10-28 08:36:07 +0000

    more_functions_nested_loops

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,8 @@
 int main void
 int _isdigit(int c);
 {
-c = 'A';
-printf("%c: %d\n", c, _isupper(c));
-c = 'a';
-printf("%c: %d\n", c, _isupper(c));
-return (0);
-else
+if (c >= 'A' && c <= 'Z')
 return (1);
+else
+return (0);
 }
