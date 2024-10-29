2d9c4140e337d8c0f6f4c000d3a66454165dc6e6
meurtphy <erwanmpn5@gmail.com>
2024-10-29 15:00:49 +0000

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+int i;
+int j;
+int legt;
+ 
+for (i = 0; s[i] != '\0'; i++)
+{
+legt = i;
+for (j = legt - 1; 0 <= j; j--)
+_putchar(s[j]);
+}
+_putchar('/');
+}
+       
+   
