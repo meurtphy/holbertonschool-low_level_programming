6f51ccdd11fe0dc9f106541714783ed52b38a8d3
meurtphy <erwanmpn5@gmail.com>
2024-10-29 12:41:26 +0000

    1-swap.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - check the code 
- *
- * 
- */
-void reset_to_98(int *n)
+* reset_to_98 - pointeur qui met a jour la valeur
+* @n: la valeur de n devient 98
+*
+*/
+Void reset_to_98(int *n)
 {
 *n = 98;
 }
