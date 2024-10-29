15454a7462579372c2555448b4ad1e18ac3b8c53
meurtphy <erwanmpn5@gmail.com>
2024-10-29 15:28:31 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,15 @@ void print_rev(char *s)
     int i = 0;
     int legt;
 
-    // Calculer la longueur de la chaîne
+    
+
     while (s[i] != '\0')
     {
         i++;
     }
     legt = i;
 
-    // Afficher la chaîne en ordre inverse
+    
     for (i = legt - 1; i >= 0; i--)
     {
         _putchar(s[i]);
