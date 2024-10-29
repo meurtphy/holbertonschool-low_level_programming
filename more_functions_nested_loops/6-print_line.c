a5dc8affad9f4124a8c6ae8302426c6625d2c9c8
meurtphy <erwanmpn5@gmail.com>
2024-10-29 09:26:24 +0000

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,9 +3,6 @@
 /**
 * print_line - Draws a straight line in the terminal
 * @n: The number of times the character '_' should be printed
-*
-* Descritption if n is 0 or less, only a newline is printed.
-* Otherwise, '_' is printed nn times followed by a newline
 */
 void print_line(int n)
 {
@@ -15,7 +12,7 @@ if (n <= 0)
 _putchar('\n');
 }
 else
-{   
+{
 for (i = 0; i < n; i++)
 {
 _putchar('_');
@@ -24,4 +21,3 @@ _putchar('\n');
 
 }
 }
- 
