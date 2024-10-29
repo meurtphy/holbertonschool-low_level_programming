162f5b94a64c1b0c3c18ff64f3f1b45baf6abe7f
meurtphy <erwanmpn5@gmail.com>
2024-10-29 09:13:25 +0000

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - afffiche nombre de 0 a 100, remplace multiple de 3 pr fizz,
+*multiple de 5 par Buzz, et multiple des 2 par FizzBuzz
+*
+* Return: O
+*/
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i 5 % == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
+if (i < 100)
+printf(" ");
+else
+printf("\n");
+}
+return (0);
+}
