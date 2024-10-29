dc86e9c068fcbe45a995ffdd88f5d5420d9c6e34
meurtphy <erwanmpn5@gmail.com>
2024-10-29 23:45:16 +0000

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -12,12 +12,12 @@ int length = 0;
 char temp;
 while (s[length] != '\0')  
 {
-length ++;
+length++;
 }
 for (i = 0; i < length / 2; i++)
 {
 temp = s[i];
 s[i] = s[length - i - 1];
 s[length - i - 1] = temp;
-}
+} 
 }
