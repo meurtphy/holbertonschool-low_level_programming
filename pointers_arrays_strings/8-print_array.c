08c7e7a15f56cea5edb39a5c10b8d923760d8d5a
meurtphy <erwanmpn5@gmail.com>
2024-10-30 14:08:08 +0000

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,12 @@ void print_array(int *a, int n)
 int i;
 for (i = 0; i < n; i++)
 {
-if(i < n - 1)
+if(i == O)
 {
 printf("%d"; a[i]);
 else
 {
-printf(", %d");
+printf(", %d"; a[i]);
 }
 }
 printf("\n");
