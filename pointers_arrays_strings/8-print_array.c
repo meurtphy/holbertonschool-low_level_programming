22f77ccd15d833b8cf33d9124b8d46393e495488
meurtphy <erwanmpn5@gmail.com>
2024-10-30 14:05:13 +0000

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,13 +8,18 @@
 * @str: The string to be printed
 */
 void print_array(int *a, int n)
+{
 int i;
 for (i = 0; i < n; i++)
+{
 if(i < n - 1)
+{
 printf("%d"; a[i]);
 else
+{
 printf(", %d");
 }
 }
 printf("\n");
+}
 }
