4724ea0eba6d8ecb494bee3e469da97c2c2a6a7e
meurtphy <erwanmpn5@gmail.com>
2024-10-30 11:45:50 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+void _puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i+= 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
