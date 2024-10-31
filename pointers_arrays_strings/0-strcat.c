941aa88247174924b39770e4e5bc7334d15cb4c4
meurtphy <erwanmpn5@gmail.com>
2024-10-31 07:54:32 +0000

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+char *_strcat(char *dest, char *src)
+{
+int i;
+int bebe;
+while (dest[i] != '\0')
+{
+i++
+}
+while (src[bebe] = '\0')
+{
+dest[i] = src[bebe];
+i++;
+bebe++;
+   }
+desti{i]='\0'
+retunr (dest);
+}
+   
