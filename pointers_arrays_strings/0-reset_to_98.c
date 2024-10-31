df656beef481d5addd0566c2f2c7291f39d4c807
meurtphy <erwanmpn5@gmail.com>
2024-10-31 14:10:18 +0000

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,12 @@
 #include "main.h"
 
 /**
-* reset_to_98 - updates the value pointed by n to 98
-*
-* @n: pointer to an int
-* Return: Void
-*/
+ * reset_to_98 - updates the value pointed by n to 98
+ *
+ * @n: pointer to an int
+ * Return: Void
+ */
+
 
 Void reset_to_98(int *n)
 {
