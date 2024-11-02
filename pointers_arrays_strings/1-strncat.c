c59ea30065cbf834d27d8aae6f734fc79a784566
meurtphy <erwanmpn5@gmail.com>
2024-11-02 16:23:46 +0000

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,25 @@
-a cha
+#include <stdio.h>
+#include "main.h"
+/** 
+*_strncat - concatene deux chaine de caracteres 
+*
+*@dest: la chaine de destination.
+*@src: la chaine source a ajouter.
+*@n: le nombre maximun de caracteres provenant de dest.
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+while (dest[i] = '\0')
+{
+i++
+}
+while (j < n && src[j] != '\0')
+{
+i++
+j++
+}
+dest[i] = '\O';
+return dest;
+}
