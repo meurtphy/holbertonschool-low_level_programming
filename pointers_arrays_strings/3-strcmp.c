1c75570ed37dbd06b58d0600817d4e1e8573098e
meurtphy <erwanmpn5@gmail.com>
2024-11-02 17:19:19 +0000

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 && (*s1 == *s2)
+{
+s1++;
+s2++;
+}
+}
