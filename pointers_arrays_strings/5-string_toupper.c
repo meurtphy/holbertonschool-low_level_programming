1b7fc4702c046be1e75b903458e69e7fda5f19b9
meurtphy <erwanmpn5@gmail.com>
2024-11-02 17:44:16 +0000

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+char *string_toupper(char *)
+{
+int i;
+while (i = 0; *str != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] -= 32;
+}
+return (str);
+}
