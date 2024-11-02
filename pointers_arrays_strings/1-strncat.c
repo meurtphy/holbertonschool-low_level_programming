2bf1b81b8388ae5cc8b25b39865be5bc0ac6731f
meurtphy <erwanmpn5@gmail.com>
2024-11-02 16:26:05 +0000

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,15 +11,15 @@ char *_strncat(char *dest, char *src, int n)
 {
 int i = 0;
 int j = 0;
-while (dest[i] = '\0')
+while (dest[i] != '\0')
 {
-i++
+i++;
 }
 while (j < n && src[j] != '\0')
 {
-i++
-j++
+i++;
+j++;
 }
-dest[i] = '\O';
+dest[i] = '\0';
 return dest;
 }
