f4d94c6f7e89b82f65419ee74be017f2928dfaca
meurtphy <erwanmpn5@gmail.com>
2024-11-03 13:57:32 +0000

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,17 +9,20 @@
 * string with their corresponding leet (1337) representations.
 * Letters 'a'/'A', 'e'/'E', 'o'/'O', 't'/'T', and 'l'/'L' are
 * replaced with '4', '3', '0', '7', and '1' respectively.
-* 
+*
 * Return: A pointer to the resulting string
 */
-char *leet(char *str) {
-
-int i,j;	
+char *leet(char *str)
+{
+int i, j;
 char to_replace[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 char replacements[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-for (i = 0; str[i] != '\0'; i++) {
-for (j = 0; j < 10; j++) {
-if (str[i] == to_replace[j]) {
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (str[i] == to_replace[j])
+{
 str[i] = replacements[j];
 break;
 }
@@ -27,4 +30,3 @@ break;
 }
 return (str);
 }
-
