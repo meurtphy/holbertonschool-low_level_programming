220f63c4dd381f3b48f37b6d8a76e12d029a9ac1
meurtphy <erwanmpn5@gmail.com>
2024-11-03 13:39:18 +0000

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,18 +7,29 @@
 *
 * Return: A pointer to the resulting string.
 */
-
 char *cap_string(char *str)
 {
-int alpha = 0;
-int omega = 1;
-while (str[alpha] != '\0')
+int i = 0;
+int capitalize_next = 1;
+while (str[i] != '\0')
 {
-  if (str[alpha] >= 'a' && str[alpha] <= 'z')
-  else if (str[alpha] >= 'A' && str[alpha] <= 'Z')
-    i++
+if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] -= 32;
+capitalize_next = 0;
 }
-else (str[omega] != str[alpha])
+if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+{
+capitalize_next = 1;
 }
-return(str[alpha]);
+else
+{
+capitalize_next = 0;
+}
+i++;
+}
+return str;
 }
