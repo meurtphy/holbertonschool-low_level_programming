a38ff6389a418f9f87095a582554f95e4533c43e
meurtphy <erwanmpn5@gmail.com>
2024-11-03 14:02:04 +0000

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* main - check the code
+* _strcmp - Compares two strings.
+* @s1: First string.
+* @s2: Second string.
 *
-* Return: Always 0.
+* Return: Negative if `s1` < `s2`, 0 if equal, positive if `s1` > `s2`.
 */
 int _strcmp(char *s1, char *s2)
 {
@@ -12,5 +14,5 @@ while (*s1 && (*s1 == *s2))
 s1++;
 s2++;
 }
-return *(unsigned char *)s1 - *(unsigned char *)s2;
+return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
