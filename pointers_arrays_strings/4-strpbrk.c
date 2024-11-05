a3863dc9faf647ee1999c072d435fb9a898276b0
meurtphy <erwanmpn5@gmail.com>
2024-11-05 11:42:35 +0100

    4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -2,14 +2,14 @@ char *_strpbrk(char *s, char *accept)
 {
     int i, j;
 
-    for (i = 0; s[i] != '\0'; i++) // Parcourir chaque caractère de `s`
+    for (i = 0; s[i] != '\0'; i++)
     {
-        for (j = 0; accept[j] != '\0'; j++) // Parcourir chaque caractère de `accept`
+        for (j = 0; accept[j] != '\0'; j++)
         {
-            if (s[i] == accept[j]) // Si une correspondance est trouvée
-                return &s[i]; // Retourne un pointeur vers la première occurrence dans `s`
+            if (s[i] == accept[j]) 
+                return (&s[i]);
         }
     }
-    return NULL; // Si aucun caractère de `accept` n'est trouvé dans `s`
+    return (NULL);
 }
     }
