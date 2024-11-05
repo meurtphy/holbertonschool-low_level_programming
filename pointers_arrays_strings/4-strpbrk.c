97cb4ef19d9a0cd04675cc4bfc8661d668c5047b
meurtphy <erwanmpn5@gmail.com>
2024-11-05 11:38:04 +0100

    4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,17 +1,15 @@
-#include "main.h"
-
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-unsigned int _strspn(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-    int i;
-    for (i = 0; <= s; i++)
-    {
-while (s[i]=accept[i])
-return(accept);
+    int i, j;
 
+    for (i = 0; s[i] != '\0'; i++) // Parcourir chaque caractère de `s`
+    {
+        for (j = 0; accept[j] != '\0'; j++) // Parcourir chaque caractère de `accept`
+        {
+            if (s[i] == accept[j]) // Si une correspondance est trouvée
+                return &s[i]; // Retourne un pointeur vers la première occurrence dans `s`
+        }
+    }
+    return NULL; // Si aucun caractère de `accept` n'est trouvé dans `s`
+}
     }
