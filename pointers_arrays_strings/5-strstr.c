bfa9195670565108f3175060f9e311353c13a83e
meurtphy <erwanmpn5@gmail.com>
2024-11-05 12:05:03 +0100

    5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,15 +12,15 @@ char *_strstr(char *haystack, char *needle)
 {
     int i, j;
 
-    if (*needle == '\0')  // Si needle est vide, retourne haystack
+    if (*needle == '\0')
         return haystack;
 
     for (i = 0; haystack[i] != '\0'; i++) {
         for (j = 0; needle[j] != '\0'; j++) {
-            if (haystack[i + j] != needle[j])  // Vérifie la correspondance
+            if (haystack[i + j] != needle[j])
                 break;
         }
-        if (needle[j] == '\0')  // Si on a atteint la fin de needle
+        if (needle[j] == '\0')
             return &haystack[i];
     }
     return NULL;
