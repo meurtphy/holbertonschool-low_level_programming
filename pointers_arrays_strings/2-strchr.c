8c776948faf4c17971e41f10a019c2f1a45e43c3
meurtphy <erwanmpn5@gmail.com>
2024-11-05 10:56:22 +0100

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,26 +1,22 @@
 #include "main.h"
-
+#include <stddef.h>
 /**
- * _strchr - Localise un caractère dans une chaîne.
- * @s: La chaîne de caractères.
- * @c: Le caractère à localiser.
+ * _strchr - multiply numbers
+ * @s : variable for the first number
+ * @c : variable for the seconde number
  *
- * Return: Un pointeur vers la première occurrence de `c` dans `s`,
- * ou NULL si le caractère n'est pas trouvé.
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
  */
 char *_strchr(char *s, char c)
 {
-    while (*s)
-    {
-        if (*s == c)
+        while (*s != '\0')
         {
-            return s;
+                if (*s == c)
+                {
+                        return (s);
+                }
+                s++;
         }
-        s++;
-    }
-    // Si `c` est '\0', on retourne un pointeur vers celui-ci dans `s`
-    if (c == '\0')
-        return s;
-    
-    return NULL;
+        return (NULL);
 }
