6d88d89bc5f09a90bbddd48452d10d846e92dc3b
meurtphy <erwanmpn5@gmail.com>
2024-11-05 11:11:43 +0100

    4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,3 @@
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -6,14 +6,19 @@
  */
 void print_chessboard(char (*a)[8])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-        {
-            _putchar(a[i][j]); // Imprime chaque case de l'échiquier
-        }
-        _putchar('\n'); // Nouvelle ligne à la fin de chaque rangée
-    }
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if (j == 7)
+			{
+				_putchar(a[i][j]);
+				_putchar('\n');
+			}
+			else
+				_putchar(a[i][j]);
+		}
+	}
 }
