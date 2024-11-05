ba85d418247ee6c7f429852b81197729b6114627
meurtphy <erwanmpn5@gmail.com>
2024-11-05 12:10:52 +0100

    8-print_diagsums.c

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - Finds the first occurrence of the substring needle in the string haystack.
+ * @haystack: The main string to be examined.
+ * @needle: The substring to be searched.
+ *
+ * Return: A pointer to the beginning of the located substring,
+ *         or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+    int i, j;
+
+    if (*needle == '\0')  // Si needle est vide, retourne haystack
+        return haystack;
+
+    for (i = 0; haystack[i] != '\0'; i++) {
+        for (j = 0; needle[j] != '\0'; j++) {
+            if (haystack[i + j] != needle[j])  // Vérifie la correspondance
+                break;
+        }
+        if (needle[j] == '\0')  // Si on a atteint la fin de needle
+            return &haystack[i];
+    }
+    return NULL;
+}
