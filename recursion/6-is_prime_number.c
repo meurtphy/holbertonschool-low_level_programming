f53994a47c39eb1affb9a821c55063924ef31812
meurtphy <erwanmpn5@gmail.com>
2024-11-06 12:54:59 +0100

    coucou

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,17 +2,24 @@
 #include <stdio.h>
 
 /**
- * helper_prime - Vérifie si un nombre est premier en testant les diviseurs.
+ * helper_prime - Fonction récursive pour vérifier les diviseurs d'un nombre.
  * @n: Le nombre à tester.
- * @cut: Le diviseur en cours de vérification.
+ * @divisor: Le diviseur en cours de vérification.
  *
  * Return: 1 si le nombre est premier, 0 sinon.
  */
-int is_prime_number(int n)
-int cut;
-if (n <= 1)
-return (0);
-if (cut * cut > n)
-return (1);
+int helper_prime(int n, int divisor)
+{
+    if (divisor * divisor > n)
+        return (1);
+    if (n % divisor == 0)
+        return (0);
+    return (helper_prime(n, divisor + 1));
+}
 
-return int is_prime_number(apple / n; + 1));
+int is_prime_number(int n)
+{
+    if (n <= 1)
+        return (0);
+    return (helper_prime(n, 2));
+}
