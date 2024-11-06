d8cdc93c750dba7f7352198368f2a9541f9bf269
meurtphy <erwanmpn5@gmail.com>
2024-11-06 12:26:10 +0100

    coucou

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- * helper - Finds the square root of two numbers
+ * _keeper - Essaie de trouver la racine carrée de `n` en utilisant `goal`.
+ * @n: Le nombre dont on veut trouver la racine carrée.
+ * @goal: Le nombre à tester pour être la racine carrée de `n`.
  *
- * @n: The number
- * @goal: The number to test for the square root of @a
- *
- * Return: square root
+ * Return: La racine carrée de `n` ou -1 si `n` n'a pas de racine carrée naturelle.
  */
 int _keeper(int n, int goal)
 {
-if (goal * goal == n)
-return (goal);
-else if (goal * goal > n)
-return (- 1);
-return (keeper(n, goal + 1));
-return(1);
+    if (goal * goal == n)
+        return goal;
+    else if (goal * goal > n)
+        return -1;
+    return _keeper(n, goal + 1);
 }
+
+/**
+ * _sqrt_recursion - Calcule la racine carrée naturelle de `n`.
+ * @n: Le nombre dont on veut trouver la racine carrée.
+ *
+ * Return: La racine carrée de `n` ou -1 si `n` n'a pas de racine carrée naturelle.
+ */
 int _sqrt_recursion(int n)
 {
     if (n < 0)
         return -1;
-    return keeper(n, 1);
+    return _keeper(n, 1);
 }
