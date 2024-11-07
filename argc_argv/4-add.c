342209d5ad349166b9f4842aedc505f3866a24c7
meurtphy <erwanmpn5@gmail.com>
2024-11-07 15:09:09 +0100

    1-args.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+    int sum = 0;
+
+    if (argc == 1)
+    {
+        printf("0\n");
+        return 0;
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        for (int j = 0; argv[i][j] != '\0'; j++)
+        {
+            if (!isdigit(argv[i][j]))
+            {
+                printf("Error\n");
+                return 1;
+            }
+        }
+        sum += atoi(argv[i]);
+    }
+
+    printf("%d\n", sum);
+    return 0;
+}
