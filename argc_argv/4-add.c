1abdc3b1be2fe057b925a33e1e277218a1ea89d5
meurtphy <erwanmpn5@gmail.com>
2024-11-07 15:53:44 +0100

    couccou2

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,16 +12,16 @@
 int main(int argc, char *argv[])
 {
 int sum = 0;
-
+int i, j;
 if (argc == 1)
 {
 printf("0\n");
 return (0);
 }
 
-for (int i = 1; i < argc; i++)
+for (i = 1; i < argc; i++)
 {
-for (int j = 0; argv[i][j] != '\0'; j++)
+for (j = 0; argv[i][j] != '\0'; j++)
 {
 if (!isdigit(argv[i][j]))
 {
