d9ce52e509ae01d551c7f0035b2667fbc7eecc0e
meurtphy <erwanmpn5@gmail.com>
2024-11-07 13:14:35 +0100

    1-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.>
+/**
+ * main - Prints all arguments it receives, each on a new line.
+ * @argc: The number of arguments.
+ * @argv: Array of arguments.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+int i;
+if (i = 0; i < argc; i++)
+{
+printf("%s/n", argc[i] );
+}
+return (0);
+}
