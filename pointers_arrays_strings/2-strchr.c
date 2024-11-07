f594bca62dd90bd73cbe4a487e58151275c1a4a2
meurtphy <erwanmpn5@gmail.com>
2024-11-07 16:04:07 +0100

    gg

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,21 +2,22 @@
 #include <stddef.h>
 /**
  * _strchr - multiply numbers
- * @s : variable for the first number
- * @c : variable for the seconde number
+ * @s: variable for the first number
+ * @c: variable for the seconde number
  *
  * Descritpion : multiply variable a and variable b
  * Return: return 0
  */
 char *_strchr(char *s, char c)
 {
-        while (*s != '\0')
-        {
-                if (*s == c)
-                {
-                        return (s);
-                }
-                s++;
-        }
-        return (NULL);
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return (NULL);
+
 }
