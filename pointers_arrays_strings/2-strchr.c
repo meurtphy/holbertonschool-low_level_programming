ea3dfffc4ac4f3a9d2b8fe973b9ee48d1107e6b8
meurtphy <erwanmpn5@gmail.com>
2024-11-08 12:29:07 +0100

    d

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,23 @@
 #include "main.h"
+#include <stddef.h>
 /**
- * _strchr - multiply numbers
- * @s: variable for the first number
- * @c: variable for the seconde number
- *
- * Descritpion : multiply variable a and variable b
- * Return: return 0
+ * _strchr - trouve chaine de caractere
+ * @s: chaine a trouver
+ * @c: caractere a trouver
+ * Return: si c est trouver, pointeur vers premiere instance
+ *         NULL si rien trouver
  */
 char *_strchr(char *s, char c)
 {
 while (*s != '\0')
 {
 if (*s == c)
-{
 return (s);
-}
 s++;
 }
-return (0);
+if (c == '\0')
+return (s);
+
+return (NULL);
 
 }
