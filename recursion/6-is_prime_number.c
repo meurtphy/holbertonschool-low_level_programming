7b06d3f7fcd66567abea6383c423a1b03fe4116e
meurtphy <erwanmpn5@gmail.com>
2024-11-08 12:14:20 +0100

    m

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,16 +10,22 @@
  */
 int helper_prime(int n, int divisor)
 {
-    if (divisor * divisor > n)
-        return (1);
-    if (n % divisor == 0)
-        return (0);
-    return (helper_prime(n, divisor + 1));
+if (divisor * divisor > n)
+return (1);
+if (n % divisor == 0)
+return (0);
+return (helper_prime(n, divisor + 1));
 }
-
+/**
+ * is_prime_number - Determines if a number is prime.
+ * @n: The number to check.
+ *
+ * Return: 1 if prime, 0 if not.
+ */
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return (0);
-    return (helper_prime(n, 2));
+if (n <= 1)
+return (0);
+return (helper_prime(n, 2));
+
 }
