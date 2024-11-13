6085e634a4a2c0b1aaf013e4aeb7b8054b564eb7
meurtphy <erwanmpn5@gmail.com>
2024-11-13 14:29:10 +0100

    0

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -10,10 +10,11 @@ int *array_range(int min, int max)
 {
 int *arr;
 int size, i;
+
 if (min > max)
 return (NULL);
 size = max - min + 1;
-arr = malloc(size * sizeof(int));
+arr = malloc(size *sizeof(int));
 if (arr == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
