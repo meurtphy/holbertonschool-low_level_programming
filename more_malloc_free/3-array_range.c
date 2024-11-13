36bc1db5978cb84f4f85d22a74ab10b26356c50a
meurtphy <erwanmpn5@gmail.com>
2024-11-13 13:58:08 +0100

    ff

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,13 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * array_range - crée un tableau d'entiers allant de min à max.
+ * @min: valeur minimale
+ * @max: valeur maximale
  *
- * Return: Nothing.
+ * Return: Pointeur vers le tableau, ou NULL en cas d'échec
  */
 int *array_range(int min, int max)
+{
+    int *array;
+    int i;
+
+    if (min > max)
+        return (NULL);
+
+
+array = malloc((max - min + 1) * sizeof(int));
+if (array == NULL)  // Vérification d'échec de malloc
+return (NULL);
+for (i = 0; min <= max; i++, min++)
+array[i] = min;
+
+return (array);
+}
+
