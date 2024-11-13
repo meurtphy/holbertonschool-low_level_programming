41d9a6d23f53e26515c08423510f3d090593cf4e
meurtphy <erwanmpn5@gmail.com>
2024-11-13 14:24:15 +0100

    0

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,28 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * array_range - crée un tableau d'entiers allant de min à max.
- * @min: valeur minimale
- * @max: valeur maximale
- *
- * Return: Pointeur vers le tableau, ou NULL en cas d'échec
+ * array_range - creer tableau d entier
+ * @min: valeur mini
+ * @max: valeur max
+ * Return: pointeur vers new tabl,NULL si echec
  */
 int *array_range(int min, int max)
 {
-int *array;
-int i;
-
+int *arr;
+int size, i;
 if (min > max)
 return (NULL);
-
-
-array = malloc((max - min + 1) * sizeof(int));
-if (array == NULL)
-for (i = 0; min <= max; i++, min++)
-array[i] = min;
-
-return (array);
+size = max - min + 1;
+arr = malloc(size * sizeof(int));
+if (arr == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+arr[i] = min + i;
+return (arr);
 }
 
