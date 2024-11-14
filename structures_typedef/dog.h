7c77192c557d6fbb158da51b831e5cf6d586f6e9
meurtphy <erwanmpn5@gmail.com>
2024-11-14 12:44:42 +0100

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure for dog information
+ * @name: Name of the dog (string)
+ * @age: Age of the dog (float)
+ * @owner: Name of the dog's owner (string)
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog;
+
+#endif /* DOG_H */
