50479747b23d44a3a490d5e05b69653d0095c685
meurtphy <erwanmpn5@gmail.com>
2024-11-15 21:48:23 +0100

    dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,61 +2,36 @@
 #include <stdlib.h>
 
 /**
- * new_dog - makes a dog
- *
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
- *
- * Return: pointer to dog
+ * new_dog - entry point
+ * @name: string from main, name of pet
+ * @age: number from main, age of pet
+ * @owner: string from main, owner of pet
+ * Return: p
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	int len;
-	char *ptr;
-
-	if (name == 0 || owner == 0)
+	dog_t *p;
+	/* reserving memory to struct*/
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
 		return (NULL);
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
-		return (NULL);
-
-	for (len = 1, ptr = name; *ptr; len++)
-		ptr++;
-	d->name = malloc(len);
-	if (d->name == 0)
+	/* Cpunting name pointer*/
+	if (name == NULL)
 	{
-		free(d);
+		free(p);
+		free(owner);
 		return (NULL);
 	}
-
-	for (len = 1, ptr = owner; *ptr; len++)
-		ptr++;
-	d->owner = malloc(len);
-	if (d->owner == 0)
+	if (owner == NULL)
 	{
-		free(d->name);
-		free(d);
+		free(p);
+		free(name);
 		return (NULL);
 	}
-
-	for (len = 0; *name != 0; len++, name++)
-		d->name[len] = *name;
-	d->name[len] = 0;
-	for (len = 0; *owner != 0; len++)
-		d->owner[len] = *owner++;
-	d->owner[len] = 0;
-	d->age = age;
-
-	return (d);
-}
-
-
-
-
-
-
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
 }
 
 
