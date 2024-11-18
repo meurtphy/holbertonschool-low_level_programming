f33710e99fc0738a9b3ed68a75e41c190c5b40c8
meurtphy <erwanmpn5@gmail.com>
2024-11-18 14:13:42 +0100

    function_pointers

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,46 +1,12 @@
-
-Analysons ensemble votre code en suivant les instructions et en utilisant une approche de réflexion pour identifier l'erreur et la corriger.
-
-Ce que la tâche demande
-À droite, la tâche demande d'écrire une fonction print_name qui prend deux paramètres :
-
-Un char *name : il s'agit d'un pointeur vers une chaîne de caractères qui représente le nom à imprimer.
-Un void (*f)(char *) : un pointeur vers une fonction qui prend un char * en paramètre et ne retourne rien (void). Cette fonction est appelée pour effectuer l'action (dans ce cas, probablement pour afficher le nom).
-Le prototype est :
-
-c
-Copier le code
-void print_name(char *name, void (*f)(char *));
-Le but est d'appeler la fonction pointée par f, en lui passant name comme argument.
-
-Analyse de votre code à gauche
-Dans votre code, vous avez essayé d'implémenter cette fonction, mais il y a quelques erreurs dans la logique :
-
-Initialisation de char i; : Vous avez déclaré char i; comme une variable locale, mais i ne semble pas nécessaire ici. Dans ce contexte, nous n'avons pas besoin d'une variable de type char pour cette fonction, car print_name ne nécessite que d'appeler la fonction pointée par f.
-
-La boucle for : Vous avez ajouté une boucle for qui semble être utilisée pour itérer, mais ce n’est pas nécessaire ici. La tâche ne demande pas de parcourir name caractère par caractère. Au lieu de cela, il vous suffit d'appeler directement la fonction pointée par f en lui passant name.
-
-La condition if (i != name) : Ici, i est un char, alors que name est un char * (pointeur vers une chaîne de caractères). Comparer un char avec un char * n'a pas de sens dans ce contexte et pourrait même causer des erreurs de compilation.
-
-La logique correcte pour print_name
-Revenons à la logique attendue pour cette fonction. La fonction print_name est assez simple :
-
-Elle doit juste vérifier que f n'est pas NULL.
-Ensuite, elle appelle f(name);.
-Voici comment vous pourriez corriger votre code.
-
-Code corrigé
-c
-Copier le code
 #include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name using a function pointer
- * @name: the name to print
- * @f: pointer to a function that takes a char * and returns nothing
- *
- */
+* print_name - prints a name using a function pointer
+* @name: the name to print
+* @f: pointer to a function that takes a char
+*
+*/
 
 void print_name(char *name, void (*f)(char *))
 {
