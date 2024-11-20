b59162bf488893bac6efdbfafa6774d0f7e64905
meurtphy <erwanmpn5@gmail.com>
2024-11-20 15:18:25 +0100

    d

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,20 +2,27 @@
 #define DOG_H
 
 /**
- * struct dog - Structure pour les informations du chien
- * @name: Nom du chien
- * @age: Âge du chien
- * @owner: Nom du propriétaire
- */
+* struct dog - Structure pour les informations du chien
+* @name: Nom du chien
+* @age: Âge du chien
+* @owner: Nom du propriétaire
+*/
 typedef struct dog
 {
 	char *name;
+
 	float age;
+
 	char *owner;
+
 } dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
+
 #endif
+
