f91cdafff9eaa1c8e6d25ad82026eb26472cb7ab
meurtphy <erwanmpn5@gmail.com>
2024-12-06 15:38:49 +0100

    betty1

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,8 +2,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 #define BUFFER_SIZE 1024
+
+void close_file(int fd);
+void handle_error(int code, const char *message, const char *filename);
 
 /**
  * main - Copies the content of a file to another file.
@@ -13,63 +17,34 @@
  * Return: 0 on success, or exits with specific codes on failure.
  */
 int main(int argc, char *argv[])
+
 {
+
 int file_from, file_to, r, w;
 char buffer[BUFFER_SIZE];
 
-
 if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
+handle_error(97, "Usage: cp file_from file_to\n", NULL);
 
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-
+handle_error(98, "Error: Can't read from file ", argv[1]);
 
 file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (file_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(file_from);
-exit(99);
-}
+handle_error(99, "Error: Can't write to ", argv[2]);
 
 while ((r = read(file_from, buffer, BUFFER_SIZE)) > 0)
 {
 w = write(file_to, buffer, r);
 if (w != r)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(file_from);
-close(file_to);
-exit(99);
-}
+handle_error(99, "Error: Can't write to ", argv[2]);
 }
 if (r == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-close(file_from);
-close(file_to);
-exit(98);
-}
+handle_error(98, "Error: Can't read from file ", argv[1]);
 
-
-if (close(file_from) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-exit(100);
-}
-if (close(file_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-exit(100);
-}
+close_file(file_from);
+close_file(file_to);
 
 return (0);
 }
